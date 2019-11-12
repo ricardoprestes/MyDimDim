295b76a3bec51e83c25f77a75cddd492b2b16c21
Ricardo Prestes <ricardo.logan@hotmail.com>
2019-11-11 21:34:28 -0300

    Creating converters
    Create Icon and Color converter to movimentation type

---
+++ src/MyDimDim/Converters/ColorMovimentationTypeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MyDimDim.Converters
+{
+    public class ColorMovimentationTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            var statusInt = System.Convert.ToInt32(value);
+            var status = (Enums.EMovimentationType)statusInt;
+            switch (status)
+            {
+                case Enums.EMovimentationType.Expense:
+                    return Application.Current.Resources["ColorExpense"];
+                case Enums.EMovimentationType.Revenue:
+                    return Application.Current.Resources["ColorRevenue"];
+                default:
+                    return Application.Current.Resources["ColorTransfer"];
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/MyDimDim/Converters/IconMovimentationTypeConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace MyDimDim.Converters
+{
+    public class IconMovimentationTypeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                return null;
+
+            var statusInt = System.Convert.ToInt32(value);
+            var status = (Enums.EMovimentationType)statusInt;
+            switch (status)
+            {
+                case Enums.EMovimentationType.Expense:
+                    return "arrow_up";
+                case Enums.EMovimentationType.Revenue:
+                    return "arrow_down";
+                default:
+                    return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
